b1f00e9e302aaf28fbd7a383631d423c5a327434
JuanR200419 <kedipert27@gmail.com>
2024-02-23 22:58:02 -0500

    test ssh

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -14,7 +14,7 @@ public class Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("prueba de ssh");
     }
     
 }
