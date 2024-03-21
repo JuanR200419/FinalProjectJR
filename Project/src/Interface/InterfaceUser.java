8f1afdec85400b630a3f91639d2d1dce88d9fe0b
JilmarV <veloza.jilmar.2393@eam.edu.co>
2024-03-20 21:04:14 -0500

    fix))

--- Project/src/Interface/InterfaceUser.java
+++ Project/src/Interface/InterfaceUser.java
@@ -4,10 +4,16 @@
  */
 package Interface;
 
+import Models.User;
+
 /**
  *
  * @author jilssa
  */
 public interface InterfaceUser {
-    
+    public void insert(User user);
+    public int selectLogin(String email,String password);
+    public User searchAdmin(int id );
+    public void delete(int id);
+    public void update(User user);
 }
