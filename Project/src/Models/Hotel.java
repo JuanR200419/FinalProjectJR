fe3c576ab8fb34abe2bc1f3a472c81359a7e1a89
JuanR200419 <kedipert27@gmail.com>
2024-04-04 22:13:59 -0500

    feat(Implements New Brach): Create new branch for subMain

--- Project/src/Models/Hotel.java
+++ Project/src/Models/Hotel.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Models;
 
 /**
@@ -13,7 +9,7 @@ public class Hotel {
     private String nameHotel;
     private String adress;
     private String classification;
-    //hotel amenities, example: games room, jacuzzi etc.
+    //hotel ame  example: games room, jacuzzi etc.
     private String modCons;
     private String pictures;
 
